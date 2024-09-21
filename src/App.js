9b00082c4798a5d68eda97ec3b6549b435878072
EdwinFISA <efigueroas3@miumg.edu.gt>
2024-09-20 20:04:22 -0600

    menu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { Fragment } from "react";
+import AppRoutes from './AppRoutes';
+
+function App() {
+  return (
+    <Fragment>
+      <AppRoutes />
+    </Fragment>
+  );
+}
+
+export default App;
